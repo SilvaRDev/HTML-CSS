f552f6c08cd4ddef5587ed6401748ff487ebed92
Rafael Silva <104386903+SilvaRDev@users.noreply.github.com>
2025-02-26 12:03:13 -0300

    atualizei ex005

--- Exercicios/ex026/MQ005/script.js
+++ Exercicios/ex026/MQ005/script.js
@@ -4,6 +4,14 @@ btnBurger.addEventListener('click', clickMenu)
 let itens = document.querySelector('#itens')
 let menuAberto = false
 
+function mudouTamanho() {
+  if (window.innerWidth >= 768) {
+    itens.style.display = 'block'
+  } else {
+    itens.style.display = 'none'
+  }
+}
+
 function clickMenu() {
   if (itens.style.display == 'block') {
     itens.style.display = 'none'
